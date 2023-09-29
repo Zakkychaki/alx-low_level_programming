f7ebea3af14c0b07ada94f2fba3a28ad1ffbea82
zakky <info.zakariachaki@gmail.com>
2023-09-29 14:28:56 -0700

    Recrusion

--- 8-print_diagsums.c
+++ 8-print_diagsums.c
@@ -1,27 +1,20 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
- */
-void print_diagsums(int *a, int size)
-{
-	int sum1, sum2, y;
 
-	sum1 = 0;
-	sum2 = 0;
+void setCharPointer(char **ptr, char value) {
+    *ptr = &value;
+}
 
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 + a[y * size + y];
-	}
+int main() {
+    char myChar = 'A';
+    char *myCharPointer = NULL;
 
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
+    setCharPointer(&myCharPointer, myChar);
 
-	printf("%d, %d\n", sum1, sum2);
+    if (myCharPointer != NULL) {
+        printf("The value of myCharPointer is: %c\n", *myCharPointer);
+    } else {
+        printf("myCharPointer is NULL\n");
+    }
+
+    return 0;
 }
